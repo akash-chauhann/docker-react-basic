bc7110df0fd2e027ea7174dcbda565a66f0a2fe7
akash-chauhan <akashchauhan1198@gmail.com>
2023-09-16 02:45:36 +0530

    basic docker-react project

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Docker project for Akash
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
